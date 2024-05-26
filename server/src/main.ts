4d2bd4623955d4197d00e0773ae5eda64bfb89d6
Renan Moraes <renan.desenvolvimento@gmail.com>
2024-05-25 22:15:32 -0300

    ➕: Add dependency for env and swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Teste Lumi Api')
+    .setDescription('API para teste de desenvolvimento de software para a Lumi')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
+  await app.listen(configService.get('PORT'), '0.0.0.0');
+  logger.log(`Service is listening on port ${process.env.PORT}`);
 }
 bootstrap();
